b7467f2bd9d2b5b0c080bb076c662f446f1e562d
Akshat Dobriyal <akcooldobriyal@gmail.com>
2021-10-03 16:00:40 +0530

    Added Navbar.scss

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,18 +5,16 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <div className="nav">
-            <div className="nav__logo">
-                <ul>
-                    <li><img src="./Logo-fina-11 1.png" alt="arcadia" /></li>
-                </ul>
-            </div>
-            <div>
-                <ul className="nav__list">
-                    <li className="nav__list__element">Announcements</li>
-                    <li className="nav__list__element">Events</li>
-                    <li className="nav__list__element">Games</li>
-                    <li className="nav__list__element">About us</li>
-                    <li className="nav__list__element"><button className="login-btn">Login</button></li>
+            <div className="nav__header">
+                <div className="nav__header__logo">
+                    <img src="Logo-final-11 1.png" alt="arcadia" />
+                </div>
+                <ul className="nav__header__list">
+                    <li className="nav__header__list__element">Announcements</li>
+                    <li className="nav__header__list__element">Events</li>
+                    <li className="nav__header__list__element">Games</li>
+                    <li className="nav__header__list__element">About us</li>
+                    <li className="nav__header__list__element"><button className="login-btn">Login</button></li>
                 </ul>
             </div>
         </div>
