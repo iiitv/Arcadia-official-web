ce805560a80a16665ea896d49d11af9ae0ebdc8d
raghusodani <201951118@iiitvadodara.ac.in>
2021-10-03 17:30:26 +0530

    Anonymous sign-in added

--- src/Components/login.jsx
+++ src/Components/login.jsx
@@ -3,18 +3,24 @@ import { useHistory } from "react-router";
 import GoogleLogin from "react-google-login";
 export default function Login(){
     let history = useHistory(); 
-    const handleLogin = ()  => {
+    const handleLogin = (res)  => {
+        console.log(res);
         history.push("/dashboard")
+    }
+    const handleAnonymous = () => {
+        history.push("/dashboard");
     }
     return (
     <div>
         <h1>Login</h1>
         <GoogleLogin
-            clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+            clientId="1032167174701-bscvulohd5d6lpppmga4341uguhvma86.apps.googleusercontent.com"
             buttonText="Log in with Google"
             onSuccess={handleLogin}
             onFailure={handleLogin}
             cookiePolicy={'single_host_origin'}
         />
+        <h2>OR</h2>
+        <button onClick = {handleAnonymous}>Sign In Anonymously</button>
     </div>)
 }
