f26d7524ff2acbe58140e7595b554ba8f70de26e
Akshat Dobriyal <akcooldobriyal@gmail.com>
2021-10-06 02:03:08 +0530

    Added LandingPage.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React , {Component} from 'react';
 import Games from "./components/games/Games";
 import Navbar from "./components/navbar/Navbar";
+import LandingPage from "./components/landingPage/LandingPage";
 import './App.scss';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 function App(){
   return(
     <div className= "App">
-      <Navbar />
-      <Games />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route exact path="/games">
+            <Games />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
   }
