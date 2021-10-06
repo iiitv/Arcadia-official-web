c4b0aec87d927e3cbb047d6560a339af0c9b00fa
RAGHU RAJ SODANI <56201454+raghusodani@users.noreply.github.com>
2021-10-06 23:54:17 +0530

    Feat : Completed Login Page   (#9)
    Login page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React , {Component} from 'react';
-import Games from "./components/games/Games";
-import './App.css';
-class App extends Component{
-  render(){
-    return (
-      <div className= "App">
-        <Games />
-      </div>
-    );
-  }
+
+import './App.scss'
+import { Route} from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import Login from './Components/login';
+import Dashbord from './Components/dashboard';
+import LandingPage from './Components/landingPage';
+function App() {
+  return (
+    <div className="App">
+     <BrowserRouter>
+       <Route exact path = "/" component={LandingPage}/>
+       <Route path = "/login" component={Login}/>
+       <Route path = "/dashboard" component={Dashbord}/>
+     </BrowserRouter>
+     <Games />
+    </div>
+  );
 }
 
 export default App;
