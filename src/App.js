89f0ef8f6d3d3d222cd76be326c8fcf7d0ef5bd1
RAGHU RAJ SODANI <56201454+raghusodani@users.noreply.github.com>
2021-10-07 18:42:25 +0530

    added route to games, login and navbar and resolved the conflicts (#14)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-
 import './App.scss'
 import { Route} from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import Login from './Components/login';
 import Dashbord from './Components/dashboard';
 import LandingPage from './Components/landingPage';
+import Games from './Components/games/Games'
+import Navbar from './Components/navbar/Navbar'
 function App() {
   return (
     <div className="App">
+    
      <BrowserRouter>
        <Route exact path = "/" component={LandingPage}/>
+       <Route  path = "/" component={Navbar}/>
        <Route path = "/login" component={Login}/>
        <Route path = "/dashboard" component={Dashbord}/>
+       <Route path = "/games" component={Games}/>
      </BrowserRouter>
-     <Games />
     </div>
   );
 }
