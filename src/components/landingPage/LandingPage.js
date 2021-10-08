18ac02faa49ec5e35d98003fc3ef8fc4c047bff7
Akshat Dobriyal <akcooldobriyal@gmail.com>
2021-10-08 12:39:32 +0530

    feat/Added LandingPage.scss

--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -5,22 +5,43 @@ const LandingPage = () => {
     return(
         <div className="landingPage">
             <div className="landingPage__head">
-                <div className="landingPage__head__players">
-                    <p className="landingPage__head__players__text1">Players</p>
+
+                <p className="landingPage__head__players">Players</p>
+
+                <p className="landingPage__head__matches">Matches</p>
+
+                <hr className="landingPage__head__line1" />
+                
+                <p className="landingPage__head__auctions">Auctions</p>
+
+                <hr className="landingPage__head__line2" />
+
+                <p className="landingPage__head__games">Games</p>
+
+                <p className="landingPage__head__teams">Teams</p>
+
+                <div className="landingPage__head__dot">
+                    <div className="landingPage__head__dot__dot1" />
+                    <div className="landingPage__head__dot__dot1" />
+                    <div className="landingPage__head__dot__dot2" />
+                    <div className="landingPage__head__dot__dot1" />
+                    <div className="landingPage__head__dot__dot1" />
                 </div>
-                <div className="landingPage__head__matches">
-                    <p className="landingPage__head__matches__text2">Matches</p>
-                </div>
-                <div className="landingPage__head__auctions">
-                    <hr className="landingPage__head__auctions__line" />
-                    <p className="landingPage__head__auctions__text3">Auctions</p>
-                    <hr className="landingPage__head__auctions__line" />
-                </div>
-                <div className="landingPage__head__games">
-                    <p className="landingPage__head__games__text4">Games</p>
-                </div>
-                <div className="landingPage__head__teams">
-                    <p className="landingPage__head__teams__text5">Teams</p>
+            </div>
+            <hr className="landingPage__mid" />
+            
+            <div className="landingPage__content">
+                <img className="landingPage__content__arcadia" src="images/Arcadia_.png" alt="" />
+
+                <p className="landingPage__content__desc">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu non sodales neque sodales. Sapien pellentesque habitant morbi tristique senectus. Enim ut sem viverra aliquet eget. Eget nulla facilisi etiam dignissim diam quis. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo. Urna neque viverra justo nec ultrices dui. Turpis massa tincidunt dui ut ornare lectus sit amet. Amet cursus sit amet dictum. Vitae congue mauris rhoncus aenean. Arcu ac tortor dignissim convallis aenean et tortor. Euismod nisi porta lorem mollis aliquam ut porttitor. Amet aliquam id diam maecenas.
+
+In metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Commodo elit at imperdiet dui accumsan sit amet nulla. Platea dictumst quisque sagittis purus sit amet volutpat consequat. Purus in mollis nunc sed id. Auctor elit sed vulputate mi sit amet. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada. Dapibus ultrices in iaculis nunc sed augue. Sit amet mattis vulputate enim. Id eu nisl nunc mi ipsum. Lobortis feugiat vivamus at augue eget
+                </p>
+                <div className="landingPage__content__tags">
+                    <img className="landingPage__content__tags__tag1" src="images/Get your match stats.png" alt="" />
+                    <img className="landingPage__content__tags__tag2" src="images/Watch Auctions Results.png" alt="" />
+                    <img className="landingPage__content__tags__tag3" src="images/Support Your Favourite teams.png" alt="" />
                 </div>
             </div>
         </div>
